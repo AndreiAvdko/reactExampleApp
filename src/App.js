a344a1e6afae089e974866088c17a23bd0c29509
Andrei_Avdnko <andrei_avdeenko@outlook.com>
2023-10-12 08:17:23 +0300

    Отображение изменения состояния переменной на странице

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  // Каждый компонент обладает состоянием
+  // При изменении состояния React изменяет компонент перерендеривая его
+
+  
+  /* В данном примере изменение переменной не будет отображаться в заголовке
+  let likes = 5;
+
+  function increment() {
+    likes++;
+    console.log(likes);
+  }
+
   return (
     <div className="App">
-        Приложение работает!
-        <button>Приложение</button>
+        <h1>{likes}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={()=> { likes--; console.log(likes) }}>Decrement</button>
     </div>
   );
+  */
+
+
+  // В данном примере всё работает
+  const [likes, setLikes] = useState(0); // переменная в которой находится состояние отображаемого значения
+  
+  function increment() {
+    // Изменяем с помощью специального метода
+    setLikes(likes+1);
+  }
+
+  return (
+    <div className="App">
+        <h1>{likes}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={()=> { setLikes(likes-1) }}>Decrement</button>
+    </div>
+  );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,29 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-*/
-
-
-/* ReactDOM.render(
-  <div>
-      Приложение работает!
-      <button>Click button!</button>
-  </div>,
-  document.getElementById('root')
-);
-*/
 
 ReactDOM.render(
-  React.createElement('button', {
-    onClick: ()=> console.log('click button')
-  }, "Click button!"), 
+  <App/>, 
   document.getElementById('root')
 );
 
